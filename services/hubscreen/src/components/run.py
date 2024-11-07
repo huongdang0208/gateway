853bdb2ee93571598d5ee3d5ec4dc47e75732a9d
huongdang0208 <dangthithuhuong02082002@gmail.com>
2024-11-07 23:16:05 +0700

    implement API backend

--- services/hubscreen/src/components/run.py
+++ services/hubscreen/src/components/run.py
@@ -1,6 +1,10 @@
+import os
+import socket
 import threading
 import PySimpleGUI as sg
+import services.hubscreen.src.components.hubscreen_pb2 as hubscreen_pb2
 
+GUI_LISTEN_SERVICE_SOCKET = "/tmp/gui_listen_socket"
 
 def run(self):
         # self.update_ip_address()
@@ -70,3 +74,54 @@ def run(self):
             if self.pysimplegui_user_settings.get('-enable debugger-', False):
                 print("Debugger is enabled")
         self.window.close()
+        
+def listen_for_commands(self):
+    if os.path.exists(GUI_LISTEN_SERVICE_SOCKET):
+        os.remove(GUI_LISTEN_SERVICE_SOCKET)
+    # Create a Unix Domain Socket
+    server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    server_socket.bind(GUI_LISTEN_SERVICE_SOCKET)
+    server_socket.listen(1)
+    print("Listening for commands from master service...")
+    while True:
+        client_socket,_ = server_socket.accept()
+        data = client_socket.recv(1024)
+        if data:
+            command = hubscreen_pb2.Command()
+            command.ParseFromString(data)
+            print(f"Received command: {command}")
+            if (command.action == 'add' or command.action == 'delete'):
+                if command.sender == "BLE":
+                    for light in command.led_device:
+                        if command.action == 'add':
+                            self.list_lights.append(light)
+                            self.create_new_device_content_block(light, "BLE")
+                        else:
+                            self.list_lights.remove(light)
+                            self.remove_device_content_block(light, "BLE")
+                else:
+                    for sw in command.sw_device:
+                        if command.action == 'add':
+                            self.list_switches.append(sw)
+                            self.create_new_device_content_block(sw, "MQTT")
+                        else:
+                            self.list_switches.remove(sw)
+                            self.remove_device_content_block(sw, "MQTT")
+            else:
+                if command.sender == 'BLE':
+                    for light in command.led_device:
+                        light_id = light.id
+                        if command.action == "turn on":
+                            self.window[f'-{str(light_id).upper()}-OFF'].update(visible=False)
+                            self.window[f'-{str(light_id).upper()}-ON'].update(visible=True)
+                        else:
+                            self.window[f'-{str(light_id).upper()}-ON'].update(visible=False)
+                            self.window[f'-{str(light_id).upper()}-OFF'].update(visible=True)
+                else:
+                    for sw in command.sw_device:
+                        sw_id = sw.id
+                        if command.action == "turn on":
+                            self.window[f'-{str(sw_id).upper()}-TOGGLE-GRAPHIC-'].update(image_data= self.toggle_btn_on)
+                        else:
+                            self.window[f'-{str(sw_id).upper()}-TOGGLE-GRAPHIC-'].update(image_data= self.toggle_btn_off)
+        client_socket.close()
